b7dd06bb84d6618e36d57d5f02d897024fa09061
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-09 20:19:55 +0530

    added constructors to create new different types of sql expressions

--- inc/sqltoast/sql_expression.h
+++ inc/sqltoast/sql_expression.h
@@ -89,4 +89,16 @@ struct sql_expression
 	};
 };
 
+sql_expression* new_unary_sql_expr(sql_expression_type type, sql_expression* unary_val);
+
+sql_expression* new_binary_sql_expr(sql_expression_type type, sql_expression* left, sql_expression* right);
+
+sql_expression* new_between_sql_expr(sql_expression_type type, sql_expression* bounds0, sql_expression* bounds2);
+
+sql_expression* new_in_sql_expr();
+
+void insert_expr_to_in_sql_expr(sql_expression* in_expr, sql_expression* from_val);
+
+sql_expression* new_valued_sql_expr(dstring* value);
+
 #endif
