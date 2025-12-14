614b39a64376b4519f56fe802c11365194d545c4
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-14 21:24:37 +0530

    added sql_types source and right way to initialize it

--- src/sql_type.c
+++ src/sql_type.c
@@ -4,11 +4,7 @@ sql_type new_sql_type(sql_type_name type_name)
 {
 	sql_type t;
 	t.type_name = type_name;
-	t.spec[0] = -1;
-	t.spec[1] = -1;
-	t.spec[2] = -1;
-	t.spec[3] = -1;
-	t.spec[4] = -1;
+	memset(t.spec, -1, sizeof(t.spec));
 	t.with_time_zone = 0;
 	return t;
 }
