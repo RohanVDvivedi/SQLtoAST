3576bf2ac0e11c4f35c42802cc440e44ccc051d2
RohanVDvivedi <rohandvivedi@gmail.com>
2025-12-14 21:25:28 +0530

    using cutlery memset instead

--- src/sql_type.c
+++ src/sql_type.c
@@ -1,10 +1,12 @@
 #include<sqltoast/sql_type.h>
 
+#include<cutlery/cutlery_stds.h>
+
 sql_type new_sql_type(sql_type_name type_name)
 {
 	sql_type t;
 	t.type_name = type_name;
-	memset(t.spec, -1, sizeof(t.spec));
+	memory_set(t.spec, -1, sizeof(t.spec));
 	t.with_time_zone = 0;
 	return t;
 }
