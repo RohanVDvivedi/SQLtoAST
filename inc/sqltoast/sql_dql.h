827697b26a1eaa2d52722ca4459f7c06df26a0e2
RohanVDvivedi <rohandvivedi@gmail.com>
2026-01-04 23:40:42 +0530

    started structuring the dql query

--- inc/sqltoast/sql_dql.h
+++ inc/sqltoast/sql_dql.h
@@ -3,6 +3,8 @@
 
 #include<sqltoast/sql_expression.h>
 
+#include<cutlery/arraylist.h>
+
 typedef enum sql_dql_type sql_dql_type;
 enum sql_dql_type
 {
@@ -10,12 +12,103 @@ enum sql_dql_type
 };
 
 typedef struct sql_dql sql_dql;
+
+typedef struct result_alias result_alias;
+struct result_alias
+{
+	sql_expression* result_expr;
+
+	// if empty, ignore
+	dstring as;
+};
+
+typedef enum relation_input_type relation_input_type;
+enum relation_input_type
+{
+	RELATION,
+	SUB_QUERY,
+	FUNCTION,
+};
+
+typedef struct relation_input relation_input;
+struct relation_input
+{
+	relation_input_type type;
+
+	union
+	{
+		dstring relation_name;
+
+		sql_dql* sub_query;
+
+		sql_expression* function_call;
+	};
+};
+
+typedef enum join_type join_type;
+enum join_type
+{
+	INNER_JOIN,
+	LEFT_JOIN,
+	RIGHT_JOIN,
+	FULL_JOIN,
+	CROSS_JOIN,
+};
+
+typedef struct join_with join_with;
+struct join_with
+{
+	join_type type;
+
+	relation_input input;
+
+	// if empty, ignore
+	dstring as;
+
+	sql_expression* on;
+};
+
+typedef enum order_by_dir order_by_dir;
+enum order_by_dir
+{
+	ORDER_BY_ASC = 0,
+	ORDER_BY_DESC = 1,
+};
+
+typedef struct order_by order_by;
+struct order_by
+{
+	sql_expression* ordering_expr;
+
+	order_by_dir dir;
+};
+
+typedef struct sql_dql sql_dql;
 struct sql_dql
 {
 	sql_dql_type type;
 
+	// result columns (each struct is result_alias)
+	arraylist result_aliases;
+
+	// from clause
+	relation_input base_input;
+	dstring base_input_as; // if empty, ignore
+
+	// join tables (each strcut is join_with)
+	arraylist joins_with;
+
 	// where clause
 	sql_expression* where_expr;
+
+	// order by clauses (each struct is order_by)
+	arraylist ordered_by;
+
+	// expressions to group_by (each struct is sql_expression)
+	arraylist group_by;
+
+	// having clause
+	sql_expression* having_expr;
 };
 
 void destroydql(sql_dql* dql);
