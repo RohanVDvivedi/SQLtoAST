464edf37e6b89ed96c7fde62a9b883a1a9699e10
RohanVDvivedi <rohandvivedi@gmail.com>
2026-02-05 02:05:15 +0530

    minor print for debug fix

--- src/sql_dql.c
+++ src/sql_dql.c
@@ -91,13 +91,19 @@ void print_dql(const sql_dql* dql)
 
 			int clauses_printed = 0;
 
+			if(dql->select_query.projection_mode == SQL_RESULT_SET_DISTINCT)
+			{
+				if(clauses_printed != 0)
+					printf(" , ");
+				printf("DISTINCT ");
+				clauses_printed++;
+			}
+
 			if(get_element_count_arraylist(&(dql->select_query.projections)) > 0)
 			{
 				if(clauses_printed != 0)
 					printf(" , ");
 				printf("projections( ");
-				if(dql->select_query.projection_mode == SQL_RESULT_SET_DISTINCT)
-					printf("DISTINCT ");
 				for(cy_uint i = 0; i < get_element_count_arraylist(&(dql->select_query.projections)); i++)
 				{
 					if(i != 0)
