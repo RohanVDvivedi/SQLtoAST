380476ca0197f11af1aa6929851f516aa341e8a6
RohanVDvivedi <rohandvivedi@gmail.com>
2026-02-15 20:36:35 +0530

    added new ddl statement new function

--- inc/sqltoast/sql_ddl.h
+++ inc/sqltoast/sql_ddl.h
@@ -12,11 +12,42 @@ enum sql_ddl_type
 	TRUNCATE_QUERY,
 };
 
+typedef enum sql_object_type sql_object_type;
+enum sql_object_type
+{
+	SQL_DATABASE,
+	SQL_SCHEMA,
+	SQL_TABLE,
+	SQL_VIEW,
+	SQL_INDEX,
+	SQL_DOMAIN,
+	SQL_TYPE,
+	SQL_SEQUENCE,
+	SQL_TRIGGER,
+	SQL_ASSERTION,
+};
+
+typedef enum sql_drop_behavior sql_drop_behavior;
+enum sql_drop_behavior
+{
+	RESTRICT,
+	CASCADE,
+};
+
 typedef struct sql_ddl sql_ddl;
 struct sql_ddl
 {
 	sql_ddl_type type;
+
+	sql_object_type object_type;
+
+	dstring object_name;
+
+	// only used if the query context required dropping, ither directly as a drop query or as a part of alter table
+	sql_drop_behavior drop_behavior;
 };
+
+sql_ddl* new_ddl(sql_ddl_type type, sql_object_type object_type);
 
 void delete_ddl(sql_ddl* ddl);
 
--- src/sql_ddl.c
+++ src/sql_ddl.c
@@ -2,7 +2,38 @@
 
 #include<stdlib.h>
 
+sql_ddl* new_ddl(sql_ddl_type type, sql_object_type object_type)
+{
+	sql_ddl* ddl = malloc(sizeof(sql_ddl));
+
+	// every ddl query has an object name
+	init_empty_dstring(&(ddl->object_name), 0);
+
+	switch(type)
+	{
+		case CREATE_QUERY :
+		{
+			break;
+		}
+		case ALTER_QUERY :
+		{
+			break;
+		}
+		case DROP_QUERY :
+		{
+			break;
+		}
+		case TRUNCATE_QUERY :
+		{
+			break;
+		}
+	}
+
+	return ddl;
+}
+
 void delete_ddl(sql_ddl* ddl)
 {
+	deinit_dstring(&(ddl->object_name));
 	free(ddl);
 }
