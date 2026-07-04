cf7ccce5fd1c8b8ad91d3ec11f8d9d3ef74aeee0
RohanVDvivedi <rohandvivedi@gmail.com>
2026-07-04 17:37:50 +0530

    init commit for infer fucntion

--- src/sql_expression_eval.c
+++ src/sql_expression_eval.c
@@ -2170,3 +2170,471 @@ void* evaluate_sql_expr(const sql_expression* expr, const sql_expr_eval_context*
 
 	return NULL;
 }
+
+static void delete_type_internal(void* t, const sql_expr_eval_context* ec_p)
+{
+	if(t == NULL)
+		return;
+	if(t == ec_p->bool_type)
+		return;
+	ec_p->delete_type(t, ec_p);
+}
+
+void* infer_type_sql_expr(const sql_expression* expr, const sql_expr_eval_context* ec_p, int* error_code)
+{
+	switch(expr->type)
+	{
+		case SQL_MUL_INV :
+		{
+			void* c = ec_p->get_type_for_data(ec_p->one_number, ec_p, error_code);;
+			if(*error_code)
+				return NULL;
+
+			void* t = infer_type_sql_expr(expr->unary_of, ec_p, error_code);
+			if(*error_code)
+			{
+				delete_type_internal(c, ec_p);
+				return NULL;
+			}
+
+			void* res = ec_p->get_return_type_for_op_exec_callback(ec_p->div, c, t, ec_p, error_code);
+			delete_type_internal(c, ec_p);
+			delete_type_internal(t, ec_p);
+			if(*error_code)
+				return NULL;
+
+			return res;
+		}
+		case SQL_NEG :
+		{
+			void* c = ec_p->get_type_for_data(ec_p->zero_number, ec_p, error_code);;
+			if(*error_code)
+				return NULL;
+
+			void* t = infer_type_sql_expr(expr->unary_of, ec_p, error_code);
+			if(*error_code)
+			{
+				delete_type_internal(c, ec_p);
+				return NULL;
+			}
+
+			void* res = ec_p->get_return_type_for_op_exec_callback(ec_p->sub, c, t, ec_p, error_code);
+			delete_type_internal(c, ec_p);
+			delete_type_internal(t, ec_p);
+			if(*error_code)
+				return NULL;
+
+			return res;
+		}
+		case SQL_BITNOT :
+		{
+			void* t = infer_type_sql_expr(expr->unary_of, ec_p, error_code);
+			if(*error_code)
+				return NULL;
+
+			void* res = ec_p->get_return_type_for_op_exec_callback(ec_p->bit_not, t, t, ec_p, error_code); // let the user decide which param type to use of the two
+			delete_type_internal(t, ec_p);
+			if(*error_code)
+				return NULL;
+
+			return res;
+		}
+
+		case SQL_LOGNOT :
+			return ec_p->bool_type;
+
+		case SQL_ADD :
+		case SQL_SUB :
+		case SQL_MUL :
+		case SQL_DIV :
+		case SQL_MOD :
+		case SQL_BITAND :
+		case SQL_BITOR :
+		case SQL_BITXOR :
+		case SQL_LSHIFT :
+		case SQL_RSHIFT :
+		case SQL_CONCAT :
+		case SQL_LIKE :
+		{
+			void* op_exec_func = NULL;
+			switch(expr->type)
+			{
+				case SQL_ADD :
+					op_exec_func = ec_p->add;
+					break;
+				case SQL_SUB :
+					op_exec_func = ec_p->sub;
+					break;
+				case SQL_MUL :
+					op_exec_func = ec_p->mul;
+					break;
+				case SQL_DIV :
+					op_exec_func = ec_p->div;
+					break;
+				case SQL_MOD :
+					op_exec_func = ec_p->mod;
+					break;
+				case SQL_BITAND :
+					op_exec_func = ec_p->bit_and;
+					break;
+				case SQL_BITOR :
+					op_exec_func = ec_p->bit_or;
+					break;
+				case SQL_BITXOR :
+					op_exec_func = ec_p->bit_xor;
+					break;
+				case SQL_LSHIFT :
+					op_exec_func = ec_p->left_shift;
+					break;
+				case SQL_RSHIFT :
+					op_exec_func = ec_p->right_shift;
+					break;
+				case SQL_CONCAT :
+					op_exec_func = ec_p->concat;
+					break;
+				case SQL_LIKE :
+					op_exec_func = ec_p->like;
+					break;
+				default :
+					break;
+			}
+
+			void* t1 = infer_type_sql_expr(expr->left, ec_p, error_code);
+			if(*error_code)
+				return NULL;
+
+			void* t2 = infer_type_sql_expr(expr->right, ec_p, error_code);
+			if(*error_code)
+			{
+				delete_type_internal(t1, ec_p);
+				return NULL;
+			}
+
+			void* res = ec_p->get_return_type_for_op_exec_callback(op_exec_func, t1, t2, ec_p, error_code);
+			delete_type_internal(t1, ec_p);
+			delete_type_internal(t2, ec_p);
+			if(*error_code)
+				return NULL;
+
+			return res;
+		}
+
+		case SQL_LOGAND :
+		case SQL_LOGOR :
+		case SQL_LOGXOR :
+		case SQL_IS :
+			return ec_p->bool_type;
+
+		case SQL_GT :
+		case SQL_GTE :
+		case SQL_LT :
+		case SQL_LTE :
+		case SQL_EQ :
+		case SQL_NEQ :
+		{
+			void* t1 = infer_type_sql_expr(expr->left, ec_p, error_code);
+			if(*error_code)
+				return NULL;
+
+			void* t2 = NULL;
+			if(expr->cmp_rhs_quantfier == SQL_CMP_NONE)
+			{
+				t2 = infer_type_sql_expr(expr->right, ec_p, error_code);
+				if(*error_code)
+				{
+					delete_type_internal(t1, ec_p);
+					return NULL;
+				}
+			}
+			else
+			{
+				t2 = ec_p->get_type_for_sub_query(expr->right_sub_query, ec_p, error_code);
+				if(*error_code)
+				{
+					delete_type_internal(t1, ec_p);
+					return NULL;
+				}
+			}
+
+			int cc = ec_p->can_compare_types(t1, t2, ec_p, error_code);
+			delete_type_internal(t1, ec_p);
+			delete_type_internal(t2, ec_p);
+			if(*error_code)
+				return NULL;
+
+			if(cc)
+				return ec_p->bool_type;
+
+			(*error_code) = 1;
+			return NULL;
+		}
+
+		case SQL_BTWN :
+		{
+			void* b0 = infer_type_sql_expr(expr->bounds[0], ec_p, error_code);
+			if(*error_code)
+				return NULL;
+
+			void* b1 = infer_type_sql_expr(expr->bounds[1], ec_p, error_code);
+			if(*error_code)
+			{
+				delete_type_internal(b0, ec_p);
+				return NULL;
+			}
+
+			void* i = infer_type_sql_expr(expr->btwn_input, ec_p, error_code);
+			if(*error_code)
+			{
+				delete_type_internal(b0, ec_p);
+				delete_type_internal(b1, ec_p);
+				return NULL;
+			}
+
+			int cc1 = ec_p->can_compare_types(b0, i, ec_p, error_code);
+			if(*error_code)
+			{
+				delete_type_internal(b0, ec_p);
+				delete_type_internal(b1, ec_p);
+				delete_type_internal(i, ec_p);
+				if(*error_code)
+					return NULL;
+			}
+			int cc2 = ec_p->can_compare_types(b1, i, ec_p, error_code);
+			delete_type_internal(b0, ec_p);
+			delete_type_internal(b1, ec_p);
+			delete_type_internal(i, ec_p);
+			if(*error_code)
+				return NULL;
+
+			if(cc1 && cc2)
+				return ec_p->bool_type;
+
+			(*error_code) = 1;
+			return NULL;
+		}
+
+		case SQL_ADD_FLAT :
+		case SQL_MUL_FLAT :
+		case SQL_CONCAT_FLAT :
+		{
+			void* res = NULL;
+			void* op_exec_func = NULL;
+			switch(expr->type)
+			{
+				case SQL_ADD_FLAT :
+					res = ec_p->get_type_for_data(ec_p->zero_number, ec_p, error_code);
+					if(*error_code)
+						return NULL;
+					op_exec_func = ec_p->add;
+					break;
+				case SQL_MUL_FLAT :
+					res = ec_p->get_type_for_data(ec_p->one_number, ec_p, error_code);
+					if(*error_code)
+						return NULL;
+					op_exec_func = ec_p->mul;
+					break;
+				case SQL_CONCAT_FLAT :
+					void* res_data = ec_p->create_string(&get_dstring_pointing_to_literal_cstring(""), ec_p, error_code);
+					if(*error_code)
+						return NULL;
+					res = ec_p->get_type_for_data(res_data, ec_p, error_code);
+					delete_data_internal(res_data, ec_p);
+					if(*error_code)
+						return NULL;
+					op_exec_func = ec_p->concat;
+					break;
+				default :
+					break;
+			}
+
+			for(cy_uint i = 0; i < get_element_count_arraylist(&(expr->expr_list)); i++)
+			{
+				void* t = infer_type_sql_expr((const sql_expression*)get_from_front_of_arraylist(&(expr->expr_list), i), ec_p, error_code);
+				if(*error_code)
+				{
+					delete_type_internal(res, ec_p);
+					return NULL;
+				}
+
+				void* new_res = ec_p->get_return_type_for_op_exec_callback(op_exec_func, res, t, ec_p, error_code);
+				delete_type_internal(res, ec_p);
+				delete_type_internal(t, ec_p);
+				if(*error_code)
+					return NULL;
+
+				res = new_res;
+			}
+
+			return res;
+		}
+
+		case SQL_LOGAND_FLAT :
+		case SQL_LOGOR_FLAT :
+		case SQL_LOGXOR_FLAT :
+			return ec_p->bool_type;
+
+		case SQL_IN :
+		{
+			void* t1 = infer_type_sql_expr(expr->in_input, ec_p, error_code);
+			if(*error_code)
+				return NULL;
+
+			if(expr->in_sub_query)
+			{
+				void* t2 = ec_p->get_type_for_sub_query(expr->in_sub_query, ec_p, error_code);
+				if(*error_code)
+					delete_type_internal(t1, ec_p);
+
+				int cc = ec_p->can_compare_types(t1, t2, ec_p, error_code);
+				delete_type_internal(t1, ec_p);
+				delete_type_internal(t2, ec_p);
+				if(*error_code)
+					return NULL;
+
+				if(cc)
+					return ec_p->bool_type;
+
+				(*error_code) = 1;
+				return NULL;
+			}
+			else
+			{
+				for(cy_uint i = 0; i < get_element_count_arraylist(&(expr->in_expr_list)); i++)
+				{
+					void* t2 = infer_type_sql_expr((const sql_expression*)get_from_front_of_arraylist(&(expr->in_expr_list), i), ec_p, error_code);
+					if(*error_code)
+					{
+						delete_type_internal(t1, ec_p);
+						return NULL;
+					}
+
+					int cc = ec_p->can_compare_types(t1, t2, ec_p, error_code);
+					delete_type_internal(t2, ec_p);
+					if(*error_code)
+					{
+						delete_type_internal(t1, ec_p);
+						return NULL;
+					}
+
+					if(cc)
+						continue;
+
+					if(!cc)
+					{
+						(*error_code) = 1;
+						delete_type_internal(t1, ec_p);
+						return NULL;
+					}
+				}
+
+				delete_type_internal(t1, ec_p);
+				return ec_p->bool_type;
+			}
+		}
+
+		case SQL_NUM :
+		{
+			void* d = ec_p->create_number(&(expr->value), ec_p, error_code);
+			if(*error_code)
+				return NULL;
+
+			void* t = ec_p->get_type_for_data(d, ec_p, error_code);
+			delete_data_internal(d, ec_p);
+			if(*error_code)
+				return NULL;
+
+			return t;
+		}
+		case SQL_STR :
+		{
+			void* d = ec_p->create_string(&(expr->value), ec_p, error_code);
+			if(*error_code)
+				return NULL;
+
+			void* t = ec_p->get_type_for_data(d, ec_p, error_code);
+			delete_data_internal(d, ec_p);
+			if(*error_code)
+				return NULL;
+
+			return t;
+		}
+
+		case SQL_VAR :
+			return ec_p->get_type_for_variable(&(expr->value), ec_p, error_code);
+
+		case SQL_TRUE :
+		case SQL_FALSE :
+		case SQL_UNKNOWN :
+			return ec_p->bool_type;
+
+		case SQL_NULL :
+			return NULL;
+
+		case SQL_FUNCTION_CALL : // TODO
+		{
+			for(cy_uint i = 0; i < get_element_count_arraylist(&(expr->param_expr_list)); i++)
+				if(has_sub_query_in_sql_exp((const sql_expression*)get_from_front_of_arraylist(&(expr->param_expr_list), i)))
+					return 1;
+			return 0;
+		}
+
+		case SQL_CAST :
+		{
+			void* t = ec_p->get_type_for_sql_type(expr->cast_type, ec_p, error_code);
+			if(*error_code)
+				return NULL;
+
+			void* d = infer_type_sql_expr(expr->cast_expr, ec_p, error_code);
+			if(*error_code)
+			{
+				delete_type_internal(t, ec_p);
+				return NULL;
+			}
+
+			int cc = ec_p->can_cast_types(t, d, ec_p, error_code);
+			delete_type_internal(d, ec_p);
+			if(*error_code)
+			{
+				delete_type_internal(t, ec_p);
+				return NULL;
+			}
+
+			if(cc)
+				return t;
+
+			(*error_code) = 1;
+			delete_type_internal(t, ec_p);
+			return NULL;
+		}
+
+		case SQL_SUB_QUERY :
+			return ec_p->get_type_for_sub_query(expr->sub_query, ec_p, error_code);
+
+		case SQL_EXISTS :
+			return ec_p->bool_type;
+
+		case SQL_CASE : // TODO
+		{
+			if(expr->case_expr)
+				if(has_sub_query_in_sql_exp(expr->case_expr))
+					return 1;
+
+			for(cy_uint i = 0; i < get_element_count_arraylist(&(expr->when_exprs)); i++)
+			{
+				if(has_sub_query_in_sql_exp((const sql_expression*) get_from_front_of_arraylist(&(expr->when_exprs), i)))
+					return 1;
+			}
+			for(cy_uint i = 0; i < get_element_count_arraylist(&(expr->then_exprs)); i++)
+			{
+				if(has_sub_query_in_sql_exp((const sql_expression*) get_from_front_of_arraylist(&(expr->then_exprs), i)))
+					return 1;
+			}
+			if(expr->else_expr)
+				if(has_sub_query_in_sql_exp(expr->else_expr))
+					return 1;
+			return 0;
+		}
+	}
+
+	return NULL;
+}
