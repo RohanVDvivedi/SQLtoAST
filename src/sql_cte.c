e9a7147a0955725d746c0ff7c22bbbb16a3a7bd1
RohanVDvivedi <rohandvivedi@gmail.com>
2026-08-29 15:25:06 +0530

    implemented print cte function

--- src/sql_cte.c
+++ src/sql_cte.c
@@ -22,6 +22,39 @@ sql_cte* new_cte(sql_cte_type cte_type)
 	return cte;
 }
 
+void snprint_cte(dstring* str_p, const sql_cte* cte)
+{
+	concatenate_dstring(str_p, &(cte->cte_name));
+
+	if(get_element_count_arraylist(&(cte->cte_column_names)) > 0)
+	{
+		snprintf_dstring(str_p, "(");
+		for(cy_uint i = 0; i < get_element_count_arraylist(&(cte->cte_column_names)); i++)
+		{
+			if(i != 0)
+				snprintf_dstring(str_p, ",");
+			concatenate_dstring(str_p, (dstring*) get_from_front_of_arraylist(&(cte->cte_column_names), i));
+		}
+		snprintf_dstring(str_p, ")");
+	}
+
+	snprintf_dstring(str_p, " AS ");
+
+	switch(cte->cte_type)
+	{
+		case CTE_DQL :
+		{
+			snprint_dql(str_p, cte->dql_query);
+			break;
+		}
+		case CTE_DML :
+		{
+			snprint_dml(str_p, cte->dml_query);
+			break;
+		}
+	}
+}
+
 void delete_dstring(dstring* d);
 
 void delete_cte(sql_cte* cte)
