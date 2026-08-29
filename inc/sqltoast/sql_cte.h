3fedd28f57f545a0539eb481c9dfec5d886c2bf8
RohanVDvivedi <rohandvivedi@gmail.com>
2026-08-29 15:08:42 +0530

    cte structure implemented

---
+++ inc/sqltoast/sql_cte.h
@@ -0,0 +1,37 @@
+#ifndef SQL_CTE_H
+#define SQL_CTE_H
+
+#include<sqltoast/sql_dql.h>
+#include<sqltoast/sql_dml.h>
+
+#include<cutlery/arraylist.h>
+#include<cutlery/dstring.h>
+
+typedef enum sql_cte_type sql_cte_type;
+enum sql_cte_type
+{
+	CTE_DQL,
+	CTE_DML,
+};
+
+typedef struct sql_cte sql_cte;
+struct sql_cte
+{
+	dstring cte_name;
+
+	arraylist cte_column_names;
+
+	sql_cte_type cte_type;
+
+	union
+	{
+		sql_dql* dql_query;
+		sql_dml* dml_query;
+	};
+};
+
+sql_cte* new_cte(sql_cte_type cte_type);
+
+void delete_cte(sql_cte* cte);
+
+#endif
---
+++ src/sql_cte.c
@@ -0,0 +1,47 @@
+#include<sqltoast/sql_cte.h>
+
+#include<sqltoast/arraylist_deleter.h>
+
+#include<stdlib.h>
+
+sql_cte* new_cte(sql_cte_type cte_type)
+{
+	sql_cte* cte = malloc(sizeof(sql_cte));
+
+	init_empty_dstring(&(cte->cte_name), 0);
+
+	initialize_arraylist(&(cte->cte_column_names), 0);
+
+	cte->cte_type = cte_type;
+	cte->dql_query = NULL;
+	cte->dml_query = NULL;
+
+	return cte;
+}
+
+void delete_dstring(dstring* d);
+
+void delete_cte(sql_cte* cte)
+{
+	deinit_dstring(&(cte->cte_name));
+
+	delete_all_and_deinitialize_arraylist_1d(&(cte->cte_column_names), (void(*)(void*))delete_dstring);
+
+	switch(cte->cte_type)
+	{
+		case CTE_DQL :
+		{
+			if(cte->dql_query)
+				delete_dql(cte->dql_query);
+			break;
+		}
+		case CTE_DML :
+		{
+			if(cte->dml_query)
+				delete_dml(cte->dml_query);
+			break;
+		}
+	}
+
+	free(cte);
+}
