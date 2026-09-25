77cb467236788cac9593754da5cc0922d8a99212
RohanVDvivedi <rohandvivedi@gmail.com>
2026-09-25 18:04:05 +0530

    more description on how to make logical plans

--- src/logical_plan.c
+++ src/logical_plan.c
@@ -1,3 +1,21 @@
 #include<sqltoast/sql_logical_plan.h>
 
+/*
+	logical order of the sql clauses
+
+	FROM
+	JOIN
+	WHERE
+	GROUP BY / AGGREGATION
+	HAVING
+	PROJECTION
+	DISTINCT
+	UNION/INTERSECT/EXCEPT
+	ORDER BY
+	OFFSET
+	LIMIT
+	INSERT/DELETE - if DML
+	 - again the same sequence if it is returning query
+*/
+
 logical_operator* get_logical_plan_for_sql(sql* sql, schema_query_interface* sqi);
